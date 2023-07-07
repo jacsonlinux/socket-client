abeadb87b5c9be21e3f81c0e9be142bbcf30f7f4
kali <jacsonlinux@gmail.com>
2023-07-07 09:58:30 -0300

    Update code *_0

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const si = require('systeminformation');
 const net = require('node:net');
+const os = require('node:os');
 const dgram = require('node:dgram');
 
 const clientTCP = new net.Socket();
 const clientUDP = new dgram.createSocket('udp4');
 
 let refreshInterval = null;
+
+let broadcast_address = null;
 
 const getDataSystem = async () => {
     console.log('Getting static system data...');
@@ -45,7 +48,11 @@ const connectUDP = () => {
         console.log('listening UDP...');
         clientUDP.setBroadcast(true);
         refreshInterval = setInterval(() => {
-            clientUDP.send(``, 0, 0, 22222, '255.255.255.255');
+            clientUDP.send(``, 0, 0, 22222, '255.255.255.255', (err) => {
+                if (err) {
+                    console.error('Erro ao enviar o pacote de broadcast:', err);
+                }
+            });
             console.log('.')
         }, 5000);
     });
@@ -57,7 +64,7 @@ const connectUDP = () => {
         }, 5000)
     });
     clientUDP.bind(33333);
-}
+};
 
 const connectTCP = (server) => {
     console.log('Connecting TCP...');
@@ -83,4 +90,39 @@ const connectTCP = (server) => {
     });
 };
 
-connectUDP();
+// connectUDP();
+
+
+function getBroadcastAddresses() {
+    const networkInterfaces = os.networkInterfaces();
+    const broadcastAddresses = [];
+
+    // Iterar sobre todas as interfaces de rede
+    Object.keys(networkInterfaces).forEach((interfaceName) => {
+        const interfaces = networkInterfaces[interfaceName];
+        // Iterar sobre todos os endereços de cada interface de rede
+        interfaces.forEach((address) => {
+            if (address.family === 'IPv4' && !address.internal) {
+                const broadcastAddress = calculateBroadcastAddress(address.address, address.netmask);
+                broadcastAddresses.push(broadcastAddress);
+            }
+        });
+    });
+
+    return broadcastAddresses;
+}
+
+function calculateBroadcastAddress(ipAddress, subnetMask) {
+    const ipParts = ipAddress.split('.');
+    const maskParts = subnetMask.split('.');
+
+    const broadcastAddressParts = ipParts.map((part, index) => {
+        return (parseInt(part) & parseInt(maskParts[index])) | (~parseInt(maskParts[index]) & 255);
+    });
+
+    return broadcastAddressParts.join('.');
+}
+
+// Exemplo de uso
+const broadcastAddresses = getBroadcastAddresses();
+console.log('Endereços de broadcast:', broadcastAddresses);
