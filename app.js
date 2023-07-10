4bf14a90c032bd65b5d749959702d12db5646552
kali <jacsonlinux@gmail.com>
2023-07-10 09:33:02 -0300

    Update code *_0 by GPT

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 const si = require('systeminformation');
 const net = require('node:net');
 const os = require('node:os');
@@ -8,7 +9,30 @@ const clientUDP = new dgram.createSocket('udp4');
 
 let refreshInterval = null;
 
-let broadcast_address = null;
+const calculateBroadcastAddress = (ipAddress, subnetMask) => {
+    const ipParts = ipAddress.split('.');
+    const maskParts = subnetMask.split('.');
+
+    const broadcastAddressParts = ipParts.map((part, index) => {
+        return (parseInt(part) & parseInt(maskParts[index])) + (~parseInt(maskParts[index]) & 255);
+    });
+
+    return broadcastAddressParts.join('.');
+};
+
+const getBroadcastAddresses = () => {
+    const interfaces = os.networkInterfaces();
+    const broadcastAddresses = [];
+    for (const [, iface] of Object.entries(interfaces)) {
+        for (const address of iface) {
+            if (address.family === 'IPv4' && !address.internal) {
+                const broadcast = calculateBroadcastAddress(address.address, address.netmask);
+                broadcastAddresses.push(broadcast);
+            }
+        }
+    }
+    return broadcastAddresses;
+};
 
 const getDataSystem = async () => {
     console.log('Getting static system data...');
@@ -37,8 +61,10 @@ const reconnect = (reject) => {
     if (reject === null) {
         clientTCP.removeAllListeners();
         refreshInterval = setInterval(() => {
-            clientUDP.send(``, 0, 0, 22222, '255.255.255.255');
-            console.log('.')
+            getBroadcastAddresses().forEach(res => {
+                clientUDP.send(``, 0, 0, 22222, `${res}`);
+            });
+            console.log('.');
         }, 5000);
     }
 };
@@ -48,12 +74,14 @@ const connectUDP = () => {
         console.log('listening UDP...');
         clientUDP.setBroadcast(true);
         refreshInterval = setInterval(() => {
-            clientUDP.send(``, 0, 0, 22222, '255.255.255.255', (err) => {
-                if (err) {
-                    console.error('Erro ao enviar o pacote de broadcast:', err);
-                }
+            getBroadcastAddresses().forEach(res => {
+                clientUDP.send(``, 0, 0, 22222, `${res}`, (err) => {
+                    if (err) {
+                        console.error('Erro ao enviar o pacote de broadcast:', err);
+                    }
+                });
             });
-            console.log('.')
+            console.log('.');
         }, 5000);
     });
     clientUDP.on('message', (msg, server) => {
@@ -90,39 +118,157 @@ const connectTCP = (server) => {
     });
 };
 
-// connectUDP();
+connectUDP();*/
+const si = require('systeminformation');
+const net = require('node:net');
+const os = require('node:os');
+const dgram = require('node:dgram');
 
+// Criar instâncias dos clientes TCP e UDP
+const clientTCP = new net.Socket();
+const clientUDP = dgram.createSocket('udp4');
 
-function getBroadcastAddresses() {
-    const networkInterfaces = os.networkInterfaces();
-    const broadcastAddresses = [];
+let refreshInterval = null;
 
-    // Iterar sobre todas as interfaces de rede
-    Object.keys(networkInterfaces).forEach((interfaceName) => {
-        const interfaces = networkInterfaces[interfaceName];
-        // Iterar sobre todos os endereços de cada interface de rede
-        interfaces.forEach((address) => {
-            if (address.family === 'IPv4' && !address.internal) {
-                const broadcastAddress = calculateBroadcastAddress(address.address, address.netmask);
-                broadcastAddresses.push(broadcastAddress);
-            }
-        });
-    });
-
-    return broadcastAddresses;
-}
-
-function calculateBroadcastAddress(ipAddress, subnetMask) {
+// Função para calcular o endereço de broadcast
+const calculateBroadcastAddress = (ipAddress, subnetMask) => {
     const ipParts = ipAddress.split('.');
     const maskParts = subnetMask.split('.');
 
     const broadcastAddressParts = ipParts.map((part, index) => {
-        return (parseInt(part) & parseInt(maskParts[index])) | (~parseInt(maskParts[index]) & 255);
+        return (parseInt(part) & parseInt(maskParts[index])) + (~parseInt(maskParts[index]) & 255);
     });
 
     return broadcastAddressParts.join('.');
-}
+};
 
-// Exemplo de uso
-const broadcastAddresses = getBroadcastAddresses();
-console.log('Endereços de broadcast:', broadcastAddresses);
+// Função para obter os endereços de broadcast disponíveis nas interfaces de rede
+const getBroadcastAddresses = () => {
+    const interfaces = os.networkInterfaces();
+    const broadcastAddresses = [];
+
+    for (const [, iface] of Object.entries(interfaces)) {
+        for (const address of iface) {
+            if (address.family === 'IPv4' && !address.internal) {
+                const broadcast = calculateBroadcastAddress(address.address, address.netmask);
+                broadcastAddresses.push(broadcast);
+            }
+        }
+    }
+
+    return broadcastAddresses;
+};
+
+// Função assíncrona para obter os dados estáticos do sistema
+const getDataSystem = async () => {
+    console.log('Getting static system data...');
+    try {
+        let data = await si.getStaticData();
+        data.type = 'static';
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// Função assíncrona para obter os dados dinâmicos do sistema
+const getDataSystemDynamic = async () => {
+    console.log('Getting dynamic system data...');
+    try {
+        let data = await si.getDynamicData();
+        data.type = 'dynamic';
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// Função para reconectar
+const reconnect = (reject) => {
+    console.log('Reconnecting...');
+    if (reject === null) {
+        // Remover todos os ouvintes do cliente TCP
+        clientTCP.removeAllListeners();
+        // Configurar intervalo de atualização
+        refreshInterval = setInterval(() => {
+            // Enviar pacote de broadcast para cada endereço de broadcast disponível
+            getBroadcastAddresses().forEach(res => {
+                clientUDP.send(``, 0, 0, 22222, `${res}`);
+            });
+            console.log('.');
+        }, 5000);
+    }
+};
+
+// Função para estabelecer a conexão UDP
+const connectUDP = () => {
+    // Ouvinte para evento "listening"
+    clientUDP.on('listening', () => {
+        console.log('listening UDP...');
+        // Habilitar o envio de pacotes de broadcast
+        clientUDP.setBroadcast(true);
+        // Configurar intervalo de atualização
+        refreshInterval = setInterval(() => {
+            // Enviar pacote de broadcast para cada endereço de broadcast disponível
+            getBroadcastAddresses().forEach(res => {
+                clientUDP.send(``, 0, 0, 22222, `${res}`, (err) => {
+                    if (err) {
+                        console.error('Error sending broadcast packet:', err);
+                    }
+                });
+            });
+            console.log('.');
+        }, 5000);
+    });
+
+    // Ouvinte para evento "message"
+    clientUDP.on('message', (msg, server) => {
+        console.log(server);
+        clearInterval(refreshInterval);
+        setTimeout(() => {
+            connectTCP(server);
+        }, 5000);
+    });
+
+    // Associar o cliente UDP à porta 33333
+    clientUDP.bind(33333);
+};
+
+// Função para estabelecer a conexão TCP
+const connectTCP = (server) => {
+    console.log('Connecting TCP...');
+    let reject = null;
+
+    // Conectar ao servidor TCP
+    clientTCP.connect(11111, `${server.address}`, () => {
+        // Habilitar o envio de pacotes keep-alive
+        clientTCP.setKeepAlive(true, 5000);
+        console.log('TCP connection established with the server.');
+
+        // Obter dados estáticos do sistema e enviar ao servidor
+        getDataSystem().then(res => {
+            console.log('OK');
+            clientTCP.write(JSON.stringify(res));
+        }).catch(error => console.error(error));
+    });
+
+    // Ouvinte para evento "data"
+    clientTCP.on('data', chunk => {
+        reject = JSON.parse(`${chunk}`);
+        console.log('Rejected');
+    });
+
+    // Ouvinte para evento "close"
+    clientTCP.on("close", () => {
+        console.log("Connection closed");
+        reconnect(reject);
+    });
+
+    // Ouvinte para evento "error"
+    clientTCP.on("error", (err) => {
+        console.log(err);
+    });
+};
+
+// Iniciar a conexão UDP
+connectUDP();
